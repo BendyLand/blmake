102743f26d333856420d91848c1b0cf48eae2391
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-12-17 13:22:53 -0500

    Add template update script

--- blmake.lua
+++ blmake.lua
@@ -1,12 +1,12 @@
 Full_build = {
     compiler = "g++",                           -- The compiler to be used (e.g., "gcc", "clang")
     files = {"main.cpp", "utils.cpp"},          -- A list of source files to compile
-    include_dirs = {"include"},  -- Directories for include files (optional)
-    preproc_opts = {"DEBUG"},                 -- Preprocessor options (e.g., macros, include paths)
-    linker_opts = {"libs"},      -- Linker options (i.e. library paths)
+    include_dirs = {"include"},                 -- Directories for include files (optional)
+    preproc_opts = {"DEBUG"},                   -- Preprocessor options (e.g., macros, include paths)
+    linker_opts = {"libs"},                     -- Linker options (i.e. library paths)
     dependencies = {"m", "pthread"},            -- libraries to link against, e.g. {"lib1", "lib2"}
-    framework_paths = {},  -- Linker options (i.e. library paths)
-    frameworks = {},            -- libraries to link against, e.g. {"lib1", "lib2"}
+    framework_paths = {},                       -- Linker options (i.e. library paths)
+    frameworks = {},                            -- libraries to link against, e.g. {"lib1", "lib2"}
     output = "main",                            -- Output file name (optional, defaults to a.out or similar)
     optimization = "-O2",                       -- Optimization level (e.g., "-O2", "-Os")
     debugging = false,                          -- Debugging options (e.g., "-g")
@@ -16,15 +16,15 @@ Full_build = {
     lto = "-flto",                              -- Link-time optimization (e.g., "-flto")
     lang_exts = {"-std=c++20"},                 -- Language standard and extensions (e.g., "-std=c11", "-fno-exceptions")
     build_type = "Debug",                       -- or "Release"
-    env_vars = {},                            -- e.g., {CC = "gcc", CXX = "g++"}
-    src_dir = "",                 -- Source directory
-    out_dir = "build",           -- Output directory
+    env_vars = {},                              -- e.g., {CC = "gcc", CXX = "g++"}
+    src_dir = "",                               -- Source directory
+    out_dir = "build",                          -- Output directory
     hooks = {                                   -- e.g., {pre_build = "scripts/pre_build.sh"} 
         pre_build = "scripts/pre_build.sh",
         post_build = "scripts/post_build.sh"
     }, 
     custom_flags = {},                          -- e.g., {"-fno-stack-protector"}
-    tests = {},                               -- e.g., {"test_suite1", "test_suite2"}
+    tests = {},                                 -- e.g., {"test_suite1", "test_suite2"}
     documentation = "",                         -- Directory or command for generating docs
     project_metadata = {                        -- Optional: project name, version, author
         name = "BendyLand",
---
+++ regen_templates.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+
+def main():
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+        match arg:
+            case "blmake":
+                os.system(
+                    """
+				cd src/templates/blmake
+				xxd -i full_build_template.txt > full_build_template.h
+				xxd -i build_template.txt > full_build_template.h
+				xxd -i simple_build_template.txt > full_build_template.h
+				xxd -i tiny_build_template.txt > full_build_template.h
+				xxd -i test_build_template.txt > full_build_template.h
+				"""
+                )
+            case "cmake":
+                os.system(
+                    """
+				cd src/templates/cmake
+				xxd -i full_build_cmake.txt > full_build_cmake.h
+				xxd -i simple_build_cmake.txt > full_build_cmake.h
+				xxd -i tiny_build_cmake.txt > full_build_cmake.h
+				"""
+                )
+            case "premake":
+                os.system(
+                    """
+				cd src/templates/premake
+				xxd -i full_build_premake.txt > full_build_premake.h
+				xxd -i build_premake.txt > full_build_premake.h
+				xxd -i simple_build_premake.txt > full_build_premake.h
+				xxd -i tiny_build_premake.txt > full_build_premake.h
+				xxd -i test_build_premake.txt > full_build_premake.h
+				"""
+                )
+            case _:
+                print("Invalid arg. Please use blmake, cmake, or premake.")
+    else:
+        print("Usage: python3 regen_templates.py blmake|cmake|premake")
+
+
+if __name__ == "__main__":
+    main()
